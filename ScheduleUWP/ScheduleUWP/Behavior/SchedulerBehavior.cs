229c898e6fc0cfec5ed1040f67bd74559b1e29f7
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-19 18:12:24 +0530

    UWP-10847 - Sample and screenshot added

---
+++ ScheduleUWP/ScheduleUWP/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Syncfusion.UI.Xaml.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace ScheduleUWP
+{
+    public class SchedulerBehavior : Behavior<SfSchedule>
+    {
+        SfSchedule schedule;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            schedule = this.AssociatedObject;
+            ObservableCollection<DateTime> dateTimes = new ObservableCollection<DateTime>();
+            dateTimes.Add(new DateTime(2021, 01, 4));
+            dateTimes.Add(new DateTime(2021, 01, 5));
+            dateTimes.Add(new DateTime(2021, 01, 6));
+            dateTimes.Add(new DateTime(2021, 01, 7));
+            dateTimes.Add(new DateTime(2021, 01, 8));
+            dateTimes.Add(new DateTime(2021, 01, 9));
+            dateTimes.Add(new DateTime(2021, 01, 10));
+            schedule.ScheduleDateRange = dateTimes;
+            this.AssociatedObject.SizeChanged += Schedule_SizeChanged;
+        }
+        private void Schedule_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.AssociatedObject.IntervalHeight = e.NewSize.Width / 14;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SizeChanged -= Schedule_SizeChanged;
+            this.schedule = null;
+        }
+    }
+}
+
+        
+    
+  
